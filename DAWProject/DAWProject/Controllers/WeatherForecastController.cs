e6e2cf9bf91faffca83174415586270d51269d8a
Mihaela Tudor <mihaela.tudor@softbinator.com>
2020-10-20 18:33:40 +0300

    Added basic controllers

--- DAWProject/DAWProject/Controllers/WeatherForecastController.cs
+++ DAWProject/DAWProject/Controllers/WeatherForecastController.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using DAWProject;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace DAWProject.Controllers
+namespace Lab1.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -23,6 +24,12 @@ namespace DAWProject.Controllers
             _logger = logger;
         }
 
+        [HttpGet("special")]
+        public string[] GetSpecial()
+        {
+            return Summaries;
+        }
+
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -35,5 +42,40 @@ namespace DAWProject.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] WeatherForecast sendObject)
+        {
+            if (sendObject != null)
+            {
+                return Ok(new
+                {
+                    Success = true,
+                    Message = "Successfully received the object"
+                });
+            }
+
+            else return BadRequest("The object is null");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody] WeatherForecast nameToDelete)
+        {
+            ArrayList list = new ArrayList
+            {
+                "a1",
+                "b2",
+                "c3",
+                "d4",
+                "e5",
+                "f6"
+            };
+
+            list.Remove(nameToDelete.Summary);
+
+            return Ok(list);
+
+        }
+
     }
 }
